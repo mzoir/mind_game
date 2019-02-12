ca3cc8a4c494e0ecd71f636eab9175d459da5929
mzoir <47563024+mzoir@users.noreply.github.com>
2019-02-12 20:19:05 +0100

    mind.py
    juste type: python mind.py

---
+++ mind.py
@@ -0,0 +1,37 @@
+print("""\033[1;32;40mHELLO
+IN FIRST PROGRAM IN
+PYTHON,THIS IS
+SIMLPE
+        PROGRAMMATION  BASED ON VERY SIMPLE
+COMPOSITION,I
+told about this games you must to try be
+intellegent\n""")
+print('---+---','HELLO','----++++')
+print("""''''''++++++++
+                +++++-------------------;;;
+  '''''''''''hello ++++++ hello """)
+  print("\033[1;32;40mhello all of you\n")
+print("""in our games it
+very hard try to be
+intellegente""")
+question = input("""\033[1;32;41mwhat is your a$
+print(True)
+question2 = input('what is your love,answer=')
+print(False)
+question3 = input('what is your god,answer=')
+print(False)
+print('hhhhhh not our god but your only god')
+
+
+
+edited = 'edited and founder by mzoir'
+print(edited)
+
+x = 10
+answers = input('if x > 5 and x < 11')
+print(x)
+print('----','+');print('m');print('----','+');$
+print('----','+');print('z');print('----','+');$
+print('----','+','----','+');print('o')
+print('----','+','----','+');print('i')
+print('----','+','----','+');print('r')
