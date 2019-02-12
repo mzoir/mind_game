33163fba10e68b17c6cb31af16dd8da88ea7e7b0
mzoir <47563024+mzoir@users.noreply.github.com>
2019-02-12 21:32:37 +0100

    mind11.py

---
+++ mind11.py
@@ -0,0 +1,95 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+
+print("""
+______              _     
+ _           _______
+#   |  ____|            
+(_)    | |         |__   
+__|
+#   | |__ ___  ___   ___ 
+_  ___| |_ _   _     | | 
+___  __ _ _ __ ___
+#   |  __/ __|/ _ \ / __| 
+|/ _ \ __| | | |    | |/ 
+_ \/ _` | '_ ` _ \
+#   | |  \__ \ (_) | (__| 
+|  __/ |_| |_| |    | |  
+__/ (_| | | | | | |
+#   |_|  |___/\___/ 
+\___|_|\___|\__|\__, |    
+|_|\___|\__,_|_| |_| |_|
+#                         
+           __/ |
+#                         
+          |___/#
+
+""")
+
+
+
+
+
+
+
+print("""\033[1;32;40mHELLO 
+IN FIRST PROGRAM IN 
+PYTHON,THIS IS 
+SIMLPE
+        PROGRAMMATION  BASED ON VERY SIMPLE 
+COMPOSITION,I 
+told about this games you must to try be 
+intellegent\n""")
+              
+
+
+
+
+print('---+---','HELLO','----++++')
+print("""''''''++++++++    
+                +++++-------------------;;;
+  '''''''''''hello ++++++ hello """)
+
+
+
+
+
+                   
+
+                                          
+print("\033[1;32;40mhello all of you\n")
+print("""in our games it 
+very hard try to be 
+intellegente""")  
+question = input("""\033[1;32;41mwhat is your action now?,answer=""") 
+print(True)                        
+question2 = input('what is your love,answer=')
+print(False)
+question3 = input('what is your god,answer=')
+print(False)
+print('hhhhhh not our god but your only god')
+
+edited = 'edited and founder by mzoir'
+print(edited)
+answers = input('if x > 5 and x < 11')
+print(x)
+print('----','+');print('m');print('----','+');print('----','+','----','+')
+print('----','+');print('z');print('----','+');print('----','+','----','+')
+print('----','+','----','+');print('o')
+print('----','+','----','+');print('i')
+print('----','+','----','+');print('r')
+
